5fe6a6e766e7d602a99ff5ca5b285c22c2fb953f
João Marcos Figueira <jmfigueira@outlook.com>
2018-06-19 22:52:49 -0300

    - Melhoria na imagem. - Leitura de "Circulos" na imagem.

--- jogodavelha.cpp
+++ jogodavelha.cpp
@@ -1,102 +1,220 @@
+#include <opencv2/core.hpp>
+#include <ocv_libs/core/resize.hpp>
+
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+#include <boost/program_options.hpp>
+
 #include <iostream>
-#include "opencv2/core.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp" 
+#include <string>
+#include <vector>
 
-using namespace cv;
-using namespace std;
-
-
-int main(int argc, const char** argv)
+enum class ContourType
 {
+	x_type,
+	o_type,
+	unknown
+};
 
-	vector <Mat> vetorIn;
-	
-	vetorIn.push_back(imread("../data/jogo1.png", IMREAD_COLOR));	
+using Contour = std::vector<cv::Point>;
 
-	int janela = 0;
-	for (auto &const im : vetorIn) {
-		
-		Mat out;
-		cvtColor(im, out, CV_BGR2GRAY);
+Contour get_approx_poly(Contour const &contour);
 
-		//Passa filtro passa alta (bordas)
-		Laplacian(out, out, CV_8U);
+std::string parse_command_line(int argc, char **argv);
 
-		//Faz o fechamento
-		Mat e = getStructuringElement(MORPH_RECT, Size(5, 5));
-		morphologyEx(out, out, CV_MOP_CLOSE, e);
+cv::Mat preprocess(cv::Mat &color_img);
 
-		//Filtro Galssiano (passa-baixa) - Suavizacao e remocao de ruidos
-		GaussianBlur(out, out, Size(3, 3), 3);
+void print_contour_properties(Contour const &contour);
 
-		//Binariza
-		threshold(out, out, 50, 255, THRESH_BINARY_INV);
+ContourType recognize_countour_type(Contour const &contour);
 
-		//Conecta os componentes com branco
-		floodFill(out, Point(0, 0), Scalar(255));
+void show_contours_properties(cv::Mat const &input,
+	std::vector<Contour> const &contour_vec);
 
-		//Aplica erosão
-		erode(out, out, e);
+void write_x_and_o(cv::Mat const &input,
+	std::vector<Contour> const &contour_vec);
 
-		//Conecta os componentes com branco
-		floodFill(out, Point(0, 0), Scalar(0));
+int main(int argc, char **argv)
+{
+	auto const ImageName = parse_command_line(argc, argv);
+	cv::Mat color_img = cv::imread(ImageName);
+	if (!color_img.empty()) {
+		cv::Mat binary_img = preprocess(color_img);
+		cv::imshow("threshold", binary_img);
+		cv::waitKey();
 
-		//Detecta os blobs
-		vector<vector<Point> > contours;
-		std::vector<cv::Vec4i> hierarchy;
-		findContours(out, contours, hierarchy, CV_RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+		std::vector<Contour> contour_vec;
+		//findContours will change the input image, provide a copy if you
+		//want to resue original image. In pyimage serach guru,
+		//it use CV_RETR_EXTERNAL, but this will omit inner
+		//contour, to detect center X we need to retrieve deeper contour
+		cv::findContours(binary_img,
+			contour_vec, CV_RETR_CCOMP,
+			CV_CHAIN_APPROX_SIMPLE);
 
-		vector<Rect> rects;
-		int maxSize = 0;
+		//show the properties of contours one by one
+		show_contours_properties(color_img, contour_vec);
 
-		//Contabiliza os blobs
-		for (int i = 0; i < contours.size(); ++i)
-		{
-			Rect rect = boundingRect(Mat(contours[i]));
-			int size = rect.size().height*rect.size().width;
-			rects.push_back(rect);
-
-			if (maxSize < size)
-				maxSize = size;
-		}
-
-		//Elimina os blobs pequenos
-		int total = 0;
-		for (auto &const rect : rects)
-		{
-			int size = rect.size().height*rect.size().width;
-
-			if (size > maxSize * 0.75)
-			{
-
-				//Cria um Mat para o blob da imagem original
-				Mat blob(Mat(out, rect));
-
-				int noZero = countNonZero(blob);
-				int size = blob.size().height * blob.size().width;
-
-				if (noZero < size * 0.95) {
-					total++;
-					rectangle(im, Rect(rect.x, rect.y, rect.width, rect.height), Scalar(0, 0, 255));
-				}
-			}
-		}
-
-
-		string win = "win";
-		win.append(to_string(janela++));
-
-		cout << "Total blobs: " << total << " win " << win << endl;
-		
-
-		imshow(win, im);
-
+		//after you observe the information of contour properties
+		//you can, it is time to recognize the x and o
+		write_x_and_o(color_img, contour_vec);
 	}
-
-	
-	waitKey(0);
-	return 0;
+	else {
+		std::cerr << "cannot open image " << ImageName << "\n";
+	}
 }
 
+Contour get_approx_poly(Contour const &contour)
+{
+	Contour poly_contour;
+	double const Epsillon = cv::arcLength(contour, true) * 0.02;
+	bool const Close = true;
+	cv::approxPolyDP(contour, poly_contour, Epsillon, Close);
 
+	return poly_contour;
+}
+
+std::string parse_command_line(int argc, char **argv)
+{
+	using namespace boost::program_options;
+
+	try
+	{
+		options_description desc{ "Options" };
+		desc.add_options()
+			//set up --help or -h to show help information
+			("help,h", "Help screen")
+			//set up --image or -i as a required argument to store the name of image
+			("image,i", value<std::string>()->required(), "Image to process");
+
+		//parse the command line and store it in containers
+		variables_map vm;
+		store(parse_command_line(argc, argv, desc), vm);
+
+		if (vm.count("help")) {
+			std::cout << desc << '\n';
+			return{};
+		}
+		else {
+			return vm["image"].as<std::string>();
+		}
+
+		//the magic to make the other required arguments
+		//become optional if the users input "help"
+		notify(vm);
+	}
+	catch (const error &ex)
+	{
+		std::cerr << ex.what() << '\n';
+	}
+
+	return{};
+}
+
+cv::Mat preprocess(cv::Mat &color_img)
+{
+	//resize the image to width 480 and keep the aspect ratio
+	ocv::resize_aspect_ratio(color_img, color_img, { 480, 0 });
+
+	//find coutours need a binary image, so we must transfer the origin img
+	//to binary image.
+	cv::Mat gray_img;
+	cv::cvtColor(color_img, gray_img, CV_BGR2GRAY);
+
+	//this would not copy the value of gray_img but create a new header
+	//I declare an alias to make code easier to read
+	cv::Mat binary_img = gray_img;
+	cv::threshold(gray_img, binary_img, 0, 255, CV_THRESH_BINARY_INV | CV_THRESH_OTSU);
+	cv::Mat const Kernel =
+		cv::getStructuringElement(cv::MORPH_RECT, { 5,5 });
+	cv::morphologyEx(binary_img, binary_img, cv::MORPH_CLOSE, Kernel);
+
+	return binary_img;
+}
+
+void print_contour_properties(Contour const &contour)
+{
+	double const ContourArea = cv::contourArea(contour);
+
+	Contour convex_hull;
+	cv::convexHull(contour, convex_hull);
+	double const ConvexHullArea = cv::contourArea(convex_hull);
+
+	auto const BoundingRect = cv::boundingRect(contour);
+	Contour const Poly = get_approx_poly(contour);
+
+	std::cout << "Contour area : " << ContourArea << std::endl;
+	std::cout << "Aspect ratio : " <<
+		(BoundingRect.width / static_cast<double>(BoundingRect.height))
+		<< std::endl;
+	std::cout << "Extend : " << ContourArea / BoundingRect.area() << std::endl;
+	std::cout << "Solidity : " << ContourArea / ConvexHullArea << std::endl;
+	std::cout << "Poly size : " << Poly.size() << ", is convex " << std::boolalpha <<
+		cv::isContourConvex(Poly) << std::endl;
+	std::cout << std::endl;
+}
+
+ContourType recognize_countour_type(Contour const &contour)
+{
+	double const ContourArea = cv::contourArea(contour);
+
+	Contour const Poly = get_approx_poly(contour);
+
+	ContourType type = ContourType::unknown;
+	if ((Poly.size() > 7 && Poly.size() < 10) && ContourArea > 1000) {
+		type = ContourType::o_type;
+	}
+	else if (Poly.size() >= 10 && ContourArea < 10000) {
+		type = ContourType::x_type;
+	}
+
+	return type;
+}
+
+void show_contours_properties(cv::Mat const &input,
+	std::vector<Contour> const &contour_vec)
+{
+	cv::Mat input_cpy;
+	for (size_t i = 0; i != contour_vec.size(); ++i) {
+		input.copyTo(input_cpy);
+		cv::Scalar const Color{ 255 };
+		int const ThickNess = 3;
+		cv::drawContours(input_cpy, contour_vec,
+			static_cast<int>(i), Color, ThickNess);
+		print_contour_properties(contour_vec[i]);
+
+		auto const WindowName = "contour " + std::to_string(i);
+		cv::imshow(WindowName, input_cpy);
+		cv::waitKey();
+		//without this line, previous contour image will not be closed
+		//before the program end
+		cv::destroyWindow(WindowName);
+	}
+}
+
+void write_x_and_o(cv::Mat const &input,
+	std::vector<Contour> const &contour_vec)
+{
+	cv::Mat input_cpy = input.clone();
+	std::string const TypeName[] = { "X", "O" };
+
+	for (size_t i = 0; i != contour_vec.size(); ++i) {
+		ContourType type = recognize_countour_type(contour_vec[i]);
+
+		if (type != ContourType::unknown) {
+			cv::Scalar const Color{ 255 };
+			int const ThickNess = 3;
+			cv::drawContours(input_cpy, contour_vec, static_cast<int>(i),
+				Color, ThickNess);
+			auto point = cv::boundingRect(contour_vec[i]).tl();
+			point.y -= 10;
+			double const Scale = 1;
+			cv::putText(input_cpy, TypeName[static_cast<int>(type)],
+				point, cv::FONT_HERSHEY_COMPLEX, Scale, Color,
+				ThickNess);
+		}
+	}
+	cv::imshow("result", input_cpy);
+	cv::waitKey();
+}
